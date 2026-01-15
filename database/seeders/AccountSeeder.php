b67adbf6909d9aed9bf1c844c34f116d4f99bc89
setsuwww <setsuprojectonly@gmail.com>
2026-01-15 22:18:00 +0700

    Push kolom NIK di Seed

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
-class AccountSeeder extends Seeder {
-    public function run(): void {
+class AccountSeeder extends Seeder
+{
+    public function run(): void
+    {
 
         User::create([
             'name' => 'Admin 1',
+            'nik' => '000000000001',
             'email' => 'admin@example.com',
             'password' => Hash::make('password'),
             'role' => 'Admin',
@@ -18,19 +21,22 @@ class AccountSeeder extends Seeder {
 
         User::create([
             'name' => 'Operator 1',
+            'nik' => '000000000002',
             'email' => 'operator@example.com',
             'password' => Hash::make('password'),
             'role' => 'Operator',
-        ]); 
+        ]);
 
         User::create([
             'name' => 'Mursidi',
+            'nik' => '000000000003',
             'email' => 'murmur@example.com',
             'password' => Hash::make('password'),
             'role' => 'User',
         ]);
         User::create([
             'name' => 'Dirman',
+            'nik' => '000000000004',
             'email' => 'dir@example.com',
             'password' => Hash::make('password'),
             'role' => 'User',
