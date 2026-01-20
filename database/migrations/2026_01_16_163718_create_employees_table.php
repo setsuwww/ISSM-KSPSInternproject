ed20e02d4fcf11939faf68f1aff4a0313131b869
setsuwww <setsuprojectonly@gmail.com>
2026-01-20 20:57:41 +0700

    employees_migrate

--- database/migrations/2026_01_16_163718_create_employees_table.php
+++ database/migrations/2026_01_16_163718_create_employees_table.php
@@ -4,22 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // bigint unsigned auto_increment primary key
+
+            $table->string('nik')->unique();
+            $table->string('nama');
+            $table->string('email');
+
+            $table->boolean('is_active')->default(true);
+
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('employees');
